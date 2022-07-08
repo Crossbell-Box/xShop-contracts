f5447be20bce13266f5f9d27e993d2c8a33f55aa
Fooooooooooox <crunchy.wafer.stick787@gmail.com>
2022-07-08 13:09:23 +0800

    feat: add some modifiers

--- src/MarketPlace.sol
+++ src/MarketPlace.sol
@@ -78,8 +78,25 @@ contract MarketPlace is IMarketPlace, Context, Initializable, MarketPlaceStorage
         _;
     }
 
-    function _validPayToken(address _payToken) internal view {
+    modifier validPayToken(
+        address _payToken
+    ) {
         require(_payToken == WCSB, "InvalidPayToken");
+        _;
+    }
+
+    modifier validDeadline(
+        uint256 _deadline
+    ) {
+        require(_deadline > _now(), "InvalidDeadline");
+        _;
+    }
+
+    modifier validPrice(
+        uint256 _price
+    ) {
+        require(_price > 0, "InvalidPrice");
+        _;
     }
 
     /**
@@ -140,13 +157,9 @@ contract MarketPlace is IMarketPlace, Context, Initializable, MarketPlaceStorage
         address _payToken,
         uint256 _price,
         uint256 _deadline
-    ) external askNotExists(_nftAddress, _tokenId, _msgSender()) {
-        _validDeadline(_deadline);
-        _validPrice(_price);
+    ) external askNotExists(_nftAddress, _tokenId, _msgSender()) validPayToken(_payToken) validDeadline(_deadline) validPrice(_price) {
         require(IERC165(_nftAddress).supportsInterface(INTERFACE_ID_ERC721), "TokenNotERC721");
         require(IERC721(_nftAddress).ownerOf(_tokenId) == _msgSender(), "NotERC721TokenOwner");
-
-        _validPayToken(_payToken);
 
         // save sell order
         askOrders[_nftAddress][_tokenId][_msgSender()] = DataTypes.Order(
@@ -174,11 +187,7 @@ contract MarketPlace is IMarketPlace, Context, Initializable, MarketPlaceStorage
         address _payToken,
         uint256 _price,
         uint256 _deadline
-    ) external askExists(_nftAddress, _tokenId, _msgSender()) {
-        _validDeadline(_deadline);
-        _validPayToken(_payToken);
-        _validPrice(_price);
-
+    ) external askExists(_nftAddress, _tokenId, _msgSender()) validPayToken(_payToken) validDeadline(_deadline) validPrice(_price) {
         DataTypes.Order storage askOrder = askOrders[_nftAddress][_tokenId][_msgSender()];
         // update sell order
         askOrder.price = _price;
@@ -258,11 +267,7 @@ contract MarketPlace is IMarketPlace, Context, Initializable, MarketPlaceStorage
         address _payToken,
         uint256 _price,
         uint256 _deadline
-    ) external bidNotExists(_nftAddress, _tokenId, _msgSender()) {
-        _validDeadline(_deadline);
-        _validPayToken(_payToken);
-        _validPrice(_price);
-
+    ) external bidNotExists(_nftAddress, _tokenId, _msgSender()) validPayToken(_payToken) validDeadline(_deadline) validPrice(_price) {
         require(IERC165(_nftAddress).supportsInterface(INTERFACE_ID_ERC721), "TokenNotERC721");
 
         // save buy order
@@ -306,11 +311,7 @@ contract MarketPlace is IMarketPlace, Context, Initializable, MarketPlaceStorage
         address _payToken,
         uint256 _price,
         uint256 _deadline
-    ) external validBid(_nftAddress, _tokenId, _msgSender()) {
-        _validDeadline(_deadline);
-        _validPayToken(_payToken);
-        _validPrice(_price);
-
+    ) external validBid(_nftAddress, _tokenId, _msgSender()) validPayToken(_payToken) validDeadline(_deadline) validPrice(_price) {
         DataTypes.Order storage bidOrder = askOrders[_nftAddress][_tokenId][_msgSender()];
         // update buy order
         bidOrder.payToken = _payToken;
@@ -365,14 +366,6 @@ contract MarketPlace is IMarketPlace, Context, Initializable, MarketPlaceStorage
 
     function _now() internal view virtual returns (uint256) {
         return block.timestamp;
-    }
-
-    function _validDeadline(uint256 _deadline) internal view {
-        require(_deadline > _now(), "InvalidDeadline");
-    }
-
-    function _validPrice(uint256 _price) internal pure {
-        require(_price > 0, "InvalidPrice");
     }
 
     /**
