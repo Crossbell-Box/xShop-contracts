215c1306943325c8156aeba66a1f9ef9fd138cfd
Albert <albert@rss3.io>
2022-08-12 17:08:55 +0800

    chore: remove src and op submodule

--- src/libraries/Constants.sol
+++
@@ -1,7 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.10;
-
-library Constants {
-    uint256 constant MAX_ROYALTY = 10000;
-    address constant NATIVE_CSB = 0xCcCCccccCCCCcCCCCCCcCcCccCcCCCcCcccccccC;
-}
