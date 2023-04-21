9cacf14db565fda39d85892ad2dc19f7589eb4f7
Albert <albert@rss3.io>
2023-04-21 11:19:52 +0800

    feat: update deploy script

--- scripts/deploySwap.ts
+++ scripts/deploySwap.ts
@@ -7,24 +7,11 @@ import { ethers } from "hardhat";
 
 async function main() {
     // update these addresses when deploying
-    const admin = "0xda2423ceA4f1047556e7a142F81a7ED50e93e160";
-    const wcsb = "0xff823B6138089Bea84E8d67fcb68f786e7Feb118";
-    const mira = "0xAfB95CC0BD320648B3E8Df6223d9CDD05EbeDC64";
-    const minMira = ethers.utils.parseEther("100");
-    const minCsb = ethers.utils.parseEther("10");
-
     const Swap = await ethers.getContractFactory("Swap");
     const swap = await Swap.deploy();
     await swap.deployed();
 
-    const Proxy = await ethers.getContractFactory("TransparentUpgradeableProxy");
-    const proxySwap = await Proxy.deploy(swap.address, admin, "0x");
-    await proxySwap.deployed();
-
-    await Swap.attach(proxySwap.address).initialize(wcsb, mira, minCsb, minMira);
-
     console.log("swap deployed to:", swap.address);
-    console.log("proxySwap deployed to:", proxySwap.address);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
